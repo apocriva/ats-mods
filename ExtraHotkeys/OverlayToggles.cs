4b569d3e22491116253fc2d1b8707c3f1cd8406f
apocriva <apocriva@gmail.com>
2024-01-13 13:17:26 -0400

    Hooking up toggles for Deposits and Blight

--- ExtraHotkeys/OverlayToggles.cs
+++ ExtraHotkeys/OverlayToggles.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BepInEx.Configuration;
 using Eremite;
+using Eremite.Buildings.UI;
 using Eremite.Controller;
 using Eremite.MapObjects.UI;
 using HarmonyLib;
@@ -11,6 +13,8 @@ namespace ExtraHotkeys;
 
 public class OverlayToggles : GameMB
 {
+    private static ConfigFile Config => Plugin.Instance.Config;
+
     private enum Overlays
     {
         Blight,
@@ -19,16 +23,21 @@ public class OverlayToggles : GameMB
         Recipe,
     }
 
+    private static ConfigEntry<bool> isToggleEnabled;
     private static Dictionary<Overlays, List<Action<InputAction.CallbackContext>>> showCallbacks = new();
     private static Dictionary<Overlays, List<Action<InputAction.CallbackContext>>> hideCallbacks = new();
     private static Dictionary<Overlays, bool> isShowing = new();
 
-    [HarmonyPatch(typeof(GameController), nameof(Eremite.Controller.GameController.StartGame))]
+    [HarmonyPatch(typeof(MainController), nameof(MainController.OnServicesReady))]
     [HarmonyPostfix]
-    private static void OnGameStarted()
+    private static void OnServicesReady()
     {
-        if (Plugin.GameObject.GetComponent<OverlayToggles>() == null)
-            Plugin.GameObject.AddComponent<OverlayToggles>();
+        isToggleEnabled = Config.Bind
+        (
+            new ConfigDefinition("Misc", "OverlayToggleEnabled"),
+            false,
+            new ConfigDescription("Set to true to enable overlay toggle functionality. False for base game behavior.")
+        );
 
         foreach (Overlays overlay in Enum.GetValues(typeof(Overlays)))
         {
@@ -38,9 +47,35 @@ public class OverlayToggles : GameMB
         }
     }
 
+    [HarmonyPatch(typeof(GameController), nameof(Eremite.Controller.GameController.StartGame))]
+    [HarmonyPostfix]
+    private static void OnGameStarted()
+    {
+        if (Plugin.GameObject.GetComponent<OverlayToggles>() == null)
+            Plugin.GameObject.AddComponent<OverlayToggles>();
+    }
+
+    [HarmonyPatch(typeof(BlightOverlaysManager), nameof(BlightOverlaysManager.OnEnable))]
+    [HarmonyPrefix]
+    private static bool Blight_OnEnable(BlightOverlaysManager __instance)
+    {
+        showCallbacks[Overlays.Blight].Add(__instance.OnHighlightStarted);
+        hideCallbacks[Overlays.Blight].Add(__instance.OnHighlightFinished);
+        return false;
+    }
+
+    [HarmonyPatch(typeof(BlightOverlaysManager), nameof(BlightOverlaysManager.OnDisable))]
+    [HarmonyPrefix]
+    private static bool Blight_OnDisable(BlightOverlaysManager __instance)
+    {
+        showCallbacks[Overlays.Blight].Remove(__instance.OnHighlightStarted);
+        hideCallbacks[Overlays.Blight].Remove(__instance.OnHighlightFinished);
+        return false;
+    }
+
     [HarmonyPatch(typeof(DepositsIndicatorsController), nameof(DepositsIndicatorsController.OnEnable))]
     [HarmonyPrefix]
-    private static bool DepositsIndicatorsController_OnEnable(DepositsIndicatorsController __instance)
+    private static bool Deposit_OnEnable(DepositsIndicatorsController __instance)
     {
         showCallbacks[Overlays.Deposit].Add(__instance.HighlightAllInput);
         hideCallbacks[Overlays.Deposit].Add(__instance.UnhighlightAllInput);
@@ -49,7 +84,7 @@ public class OverlayToggles : GameMB
 
     [HarmonyPatch(typeof(DepositsIndicatorsController), nameof(DepositsIndicatorsController.OnDisable))]
     [HarmonyPrefix]
-    private static bool DepositsIndicatorsController_OnDisable(DepositsIndicatorsController __instance)
+    private static bool Deposit_OnDisable(DepositsIndicatorsController __instance)
     {
         showCallbacks[Overlays.Deposit].Remove(__instance.HighlightAllInput);
         hideCallbacks[Overlays.Deposit].Remove(__instance.UnhighlightAllInput);
@@ -58,33 +93,63 @@ public class OverlayToggles : GameMB
 
     private void Awake()
     {
-        InputConfig.HighlightResources.performed += OnHighlightResourcesPressed;
-        InputConfig.HighlightResources.canceled += OnHighlightResourcesReleased;
+        InputConfig.BlightOverlay.performed += OnBlightPressed;
+        InputConfig.BlightOverlay.canceled += OnBlightReleased;
+        InputConfig.HighlightResources.performed += OnDepositPressed;
+        InputConfig.HighlightResources.canceled += OnDepositReleased;
     }
 
     private void OnDestroy()
     {
-        InputConfig.HighlightResources.performed -= OnHighlightResourcesPressed;
-        InputConfig.HighlightResources.canceled -= OnHighlightResourcesReleased;
+        InputConfig.BlightOverlay.performed -= OnBlightPressed;
+        InputConfig.BlightOverlay.canceled -= OnBlightReleased;
+        InputConfig.HighlightResources.performed -= OnDepositPressed;
+        InputConfig.HighlightResources.canceled -= OnDepositReleased;
     }
 
-    private void OnHighlightResourcesPressed(InputAction.CallbackContext context)
+    private static void OnBlightPressed(InputAction.CallbackContext context)
     {
-        if (isShowing[Overlays.Deposit])
+        OnPressed(Overlays.Blight, context);
+    }
+
+    private static void OnBlightReleased(InputAction.CallbackContext context)
+    {
+        OnReleased(Overlays.Blight, context);
+    }
+
+    private static void OnDepositPressed(InputAction.CallbackContext context)
+    {
+        OnPressed(Overlays.Deposit, context);
+    }
+
+    private static void OnDepositReleased(InputAction.CallbackContext context)
+    {
+        OnReleased(Overlays.Deposit, context);
+    }
+
+    private static void OnPressed(Overlays overlay, InputAction.CallbackContext context)
+    {
+        if (!isToggleEnabled.Value || !isShowing[overlay])
         {
-            isShowing[Overlays.Deposit] = false;
-            foreach (var action in hideCallbacks[Overlays.Deposit])
+            isShowing[overlay] = true;
+            foreach (var action in showCallbacks[overlay])
                 action.Invoke(context);
         }
         else
         {
-            isShowing[Overlays.Deposit] = true;
-            foreach (var action in showCallbacks[Overlays.Deposit])
+            isShowing[overlay] = false;
+            foreach (var action in hideCallbacks[overlay])
                 action.Invoke(context);
         }
     }
 
-    private void OnHighlightResourcesReleased(InputAction.CallbackContext context)
+    private static void OnReleased(Overlays overlay, InputAction.CallbackContext context)
     {
+        if (!isToggleEnabled.Value)
+        {
+            isShowing[overlay] = false;
+            foreach (var action in hideCallbacks[overlay])
+                action.Invoke(context);
+        }
     }
 }
